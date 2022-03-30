f15e9a219cd33a06acc77c58e3583d2fa72a5c71
Raul <raulmayas20@gmail.com>
2022-03-30 12:33:38 -0600

    Working on projects area

--- portfolios/urls.py
+++ portfolios/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("projects", views.projects, name="projects"),
+    path("resume", views.resume, name="resume"),
+    path("interests", views.interests, name="interests"),
 ]
--- portfolios/views.py
+++ portfolios/views.py
@@ -7,3 +7,16 @@ from django.template.loader import render_to_string
 def index(request):
 
     return render(request, "portfolios/index.html")
+
+def projects(request):
+
+    return render(request, "portfolios/projects.html")
+
+def resume(request):
+
+    return render(request, "portfolios/resume.html")
+
+def interests(request):
+
+    return render(request, "portfolios/interests.html")
+
