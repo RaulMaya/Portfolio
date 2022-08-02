2473dcbe0ee10d54b27fab54019f678ab8134eac
Raul <raulmayas20@gmail.com>
2022-08-02 12:33:57 -0500

    Populating projects page using models

--- portfolios/admin.py
+++ portfolios/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Project, Tag
 
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    # readonly_fields =  ("slug", )
+    prepopulated_fields = {"slug":("title",)}
+    list_filter = ("tag" ,"date")
+    list_display = ("title", "date")
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Tag)
--- portfolios/models.py
+++ portfolios/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.caption
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="images", null=True)
+    date = models.DateField(auto_now_add=True) # Automatically set whenever there is an update
+    slug = models.SlugField(max_length=100, unique=True, default="", db_index=True) # Unique True implies an index
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    url_link = models.URLField(max_length = 400)
+    tag = models.ManyToManyField(Tag, null=False, related_name="posts")
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+    
+
+    def __str__(self):
+        return f"{self.title}"
